d27cc09c5bf2d863f7c5418ae536f71aea8879ca
Konstantin Luzgin <konstantin.luzgin59@gmail.com>
2024-03-24 20:25:51 +0500

    Added migrations

---
+++ src/MusicPipeBot/Migrations/20240324151651_Initial.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicPipeBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserStates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TelegramId = table.Column<long>(type: "bigint", nullable: false),
+                    ConnectionPhrase = table.Column<string>(type: "text", nullable: false),
+                    CurrentState = table.Column<int>(type: "integer", nullable: false),
+                    Context = table.Column<string>(type: "jsonb", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserStates", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserStates_TelegramId",
+                table: "UserStates",
+                column: "TelegramId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserStates");
+        }
+    }
+}
